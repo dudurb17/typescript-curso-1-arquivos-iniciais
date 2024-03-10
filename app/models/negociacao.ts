e1a45a38256420e7f013a87bcda92453aeb485dd
dudurb17 <eduardo.robettibedin@gmail.com>
2024-03-10 17:25:19 -0300

    podendo chamar o metodo direto

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -11,4 +11,17 @@ export class Negociacao {
     return data
   }
 
+  public static criaDe(dataString:string, quantidadeString:string, valorString:string):Negociacao{
+    const exp=/-/g;
+    const date =new Date(dataString.replace(exp, ","))
+    const quantidade=parseInt(quantidadeString)
+    const valor=parseFloat(valorString)
+    return new Negociacao(
+        date,
+        quantidade,
+        valor
+    )
+  }
+
+
 }
