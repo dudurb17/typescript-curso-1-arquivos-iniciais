285aa6d32d7605f755976d7d8b311c50fb7ea80f
dudurb17 <eduardo.robettibedin@gmail.com>
2024-03-10 16:45:29 -0300

    colocando condiçao de apenas dias uteis

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -20,9 +20,16 @@ export class NegociacaoController{
 
     public adiciona():void{
         const negociacao=this.criaNegociacao()
-        this.negociacoes.adiciona(negociacao)
-        this.limparFormulario()
-        this.atualizaView()
+        if(negociacao.data.getDate()>0 && negociacao.data.getDay()<6 )
+        {
+            this.negociacoes.adiciona(negociacao)
+            this.limparFormulario()
+            this.atualizaView()
+        }else{
+            this.mensagemView.update("Apenas negociações em dias uteis são aceitas")
+        }
+
+
     }   
 
     private criaNegociacao():Negociacao{
