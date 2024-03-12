d33a87f23fbaca8d41d2444ffabc57fc023c18e5
dudurb17 <eduardo.robettibedin@gmail.com>
2024-03-12 10:41:20 -0300

    pegando dados do cache

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -1,9 +1,12 @@
 export function domInjector(seletor) {
     return function (target, propertykey) {
         console.log(`Modificando protype ${target.constructor.name} e adicionando getter para a propriedade ${propertykey}`);
+        let elemento;
         const getter = function () {
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando elemento do DOm com o seletor ${seletor} para injetar em ${propertykey}`);
+            if (!elemento) {
+                elemento = document.querySelector(seletor);
+                console.log(`buscando elemento do DOm com o seletor ${seletor} para injetar em ${propertykey}`);
+            }
             return elemento;
         };
         Object.defineProperty(target, propertykey, { get: getter });
--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,9 +1,15 @@
 export function domInjector(seletor:string){
     return function (target:any, propertykey:string){
         console.log(`Modificando protype ${target.constructor.name} e adicionando getter para a propriedade ${propertykey}`)
+
+        let elemento:HTMLElement;
+        
         const getter=function(){
-            const elemento=document.querySelector(seletor)
-            console.log(`buscando elemento do DOm com o seletor ${seletor} para injetar em ${propertykey}`)
+            if(!elemento){
+                elemento =<HTMLElement> document.querySelector(seletor)
+                console.log(`buscando elemento do DOm com o seletor ${seletor} para injetar em ${propertykey}`)
+            }
+         
             return elemento
         }
 
