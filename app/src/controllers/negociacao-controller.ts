4a6ee22ffe6b4c114accbcded87aab09125e84c3
dudurb17 <eduardo.robettibedin@gmail.com>
2024-03-12 21:57:50 -0300

    passando o dado para mostrar na tela

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -5,6 +5,7 @@ import { DiasDaSemana } from '../enums/dias-da-semana.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
 import { NegociacoesService } from '../services/negociacoes-services.js';
+import { imprimir } from '../utils/imprimir.js';
 import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 
@@ -41,8 +42,7 @@ export class NegociacaoController {
         }
 
         this.negociacoes.adiciona(negociacao);
-        console.log(negociacao.paraTexto())
-        console.log(this.negociacoes.paraTexto())
+        imprimir(negociacao)
         this.limparFormulario();
         this.atualizaView()
     }
---
+++ app/src/utils/imprimir.ts
@@ -0,0 +1,8 @@
+import { Negociacao } from "../models/negociacao.js";
+
+export function imprimir(...objetos:Array<Negociacao>){
+    for(let objeto of objetos){
+        console.log(objeto.paraTexto)
+    }
+
+}
